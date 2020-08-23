ef3d1f2aaf86b456c50f367cc2e21e7b9c7bd078
Nathan Jaber <nathanjaber2014@gmail.com>
2020-08-23 05:25:46 -0300

    Class IndexController deleted

--- src/main/java/com/example/cars/api/IndexController.java
+++
@@ -1,9 +0,0 @@
-package com.example.cars.api;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class IndexController {
-}
