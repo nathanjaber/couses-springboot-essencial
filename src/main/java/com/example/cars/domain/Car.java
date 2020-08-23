14c7e5a7df1c3abfecfeb0b6d2946f47af2f777d
Nathan Jaber <nathanjaber2014@gmail.com>
2020-08-22 22:32:13 -0300

    Class car created

---
+++ src/main/java/com/example/cars/domain/Car.java
@@ -0,0 +1,27 @@
+package com.example.cars.domain;
+
+public class Car {
+    private Long id;
+    private String name;
+
+    public Car(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
