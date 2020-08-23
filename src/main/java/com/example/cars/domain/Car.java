2300946c2d24867f12a43971a28ce6c2af59d1b9
Nathan Jaber <nathanjaber2014@gmail.com>
2020-08-23 02:37:37 -0300

    Car.id's generation strategy has changed from auto to identity

--- src/main/java/com/example/cars/domain/Car.java
+++ src/main/java/com/example/cars/domain/Car.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
