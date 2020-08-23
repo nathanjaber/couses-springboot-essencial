dff2625ad36a68faac0f69cd894a770b164460ec
Nathan Jaber <nathanjaber2014@gmail.com>
2020-08-22 22:45:53 -0300

    Base path /api/v1/cars defined Operation get /cars created

--- src/main/java/com/example/cars/api/CarsController.java
+++ src/main/java/com/example/cars/api/CarsController.java
@@ -1,9 +1,20 @@
 package com.example.cars.api;
 
+import com.example.cars.domain.Car;
+import com.example.cars.domain.CarService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/cars")
 public class CarsController {
+    private CarService service = new CarService();
+
+    @GetMapping()
+    public List<Car> get(){
+        return service.getCars();
+    }
 }
