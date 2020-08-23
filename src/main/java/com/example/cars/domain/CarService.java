f4e7ffef24ddb1076ec5e99e7f78b60eabcb1b50
Nathan Jaber <nathanjaber2014@gmail.com>
2020-08-22 22:37:03 -0300

    Operation getCars created, cars list manually added

--- src/main/java/com/example/cars/domain/CarService.java
+++ src/main/java/com/example/cars/domain/CarService.java
@@ -1,4 +1,17 @@
 package com.example.cars.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarService {
+
+    public List<Car> getCars() {
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car(1L, "Fusca"));
+        cars.add(new Car(2L, "Brasilia"));
+        cars.add(new Car(3L, "Chevette"));
+
+        return  cars;
+    }
 }
