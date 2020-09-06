671e11174ffa1f4d34bd5f8b53b11f8b832ab043
Nathan Jaber <nathanjaber2014@gmail.com>
2020-09-05 21:55:09 -0300

    Car's getters and setters removed, lombok annotation @Data added

--- src/main/java/com/example/cars/domain/Car.java
+++ src/main/java/com/example/cars/domain/Car.java
@@ -1,44 +1,18 @@
 package com.example.cars.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 public class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String type;
 
-    public Car(){}
-    public Car(Long id, String name, String type) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
